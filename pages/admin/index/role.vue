ae13b071634232b065d36c428b56ab904ac86f3e
GaoNeng-wWw <gaonenggg@outlook.com>
2024-12-19 21:28:02 +0800

    refactor(role): extract composable useRole hook

--- pages/admin/index/role.vue
+++ pages/admin/index/role.vue
@@ -1,173 +1,7 @@
 <script lang="ts" setup>
 import { Button as MButton } from '@miraiui-org/vue-button';
-import { useMessage } from '@miraiui-org/vue-message';
-import type { Role, Permission } from '@prisma/client';
-import type { SerializeObject } from 'nitropack/types';
-import type { VxeTableInstance } from 'vxe-table';
-import type { AppTagSelectOption } from '~/components/AppTagSelect/index.type';
 
-const role = reactive({
-  name: '',
-  desc: '',
-});
-const activeId = ref(-1);
-
-const page = ref(1);
-
-const { data: permissionData } = useFetch('/api/permission', { method: 'get', query: { all: true }, server: false });
-const { data: payload } = useFetch<PaginatedData<SerializeObject<Role>> | null>('/api/role', { method: 'get', server: false, query: { page } });
-const data = ref<SerializeObject<Role>[]>([]);
-
-const addedPermission = ref<number[]>([]);
-const permissions = computed<SerializeObject<Permission>[]>(() => permissionData.value?.data ?? []);
-const permissionsInput: ComputedRef<AppTagSelectOption<number>[]> = computed(() => {
-  return permissions.value.map<AppTagSelectOption<number>>((p) => {
-    return {
-      value: p.id,
-      label: p.name,
-    };
-  });
-});
-const table = useTemplateRef<VxeTableInstance>('table');
-const showDialog = ref(false);
-const totalItems = computed(() => payload.value?.total ?? 0);
-
-const removeUser = (id: number) => {
-  $fetch(`/api/role/${id}`, { method: 'delete' })
-    .then(() => {
-      data.value = data.value.filter(role => role.id !== id);
-    })
-    .catch((err) => {
-      useMessage({
-        content: err.data.message,
-        type: 'danger',
-      });
-    });
-};
-
-const closeDialog = () => {
-  addedPermission.value = [];
-  role.name = '';
-  role.desc = '';
-  showDialog.value = false;
-};
-
-const detailPatch = () => {
-  $fetch(
-    `/api/role/${activeId.value}`,
-    {
-      method: 'patch',
-      body: {
-        name: role.name,
-        desc: role.desc,
-        permissionIds: addedPermission.value,
-      },
-    },
-  )
-    .then((role) => {
-      const idx = data.value.findIndex(oldRole => oldRole.id === role.id);
-      if (idx === -1) {
-        data.value.unshift(role);
-        return;
-      }
-      data.value.splice(idx, 1, {
-        name: role.name,
-        desc: role.desc,
-        update_at: role.update_at,
-        create_at: role.create_at,
-        id: role.id,
-      });
-      table.value?.reloadData(data.value);
-    })
-    .finally(() => {
-      closeDialog();
-    });
-};
-
-const patchRole = ({ row }: { row: Role }) => {
-  $fetch(
-    `/api/role/${row.id}`,
-    {
-      method: 'patch',
-      body: {
-        name: row.name,
-        desc: row.desc,
-      },
-    },
-  )
-    .then((role) => {
-      const oldRoleId = data.value.findIndex(data => data.id === role.id);
-      data.value.splice(oldRoleId, 1, {
-        name: role.name,
-        desc: role.desc,
-        update_at: role.update_at,
-        create_at: role.create_at,
-        id: role.id,
-      });
-      useMessage({
-        content: '修改成功',
-      });
-    })
-    .catch((err) => {
-      useMessage({
-        type: 'danger',
-        content: err.data.message,
-      });
-    });
-};
-
-const addRole = () => {
-  $fetch(
-    '/api/role',
-    {
-      method: 'post',
-      body: {
-        name: role.name,
-        desc: role.desc,
-        permissionIds: addedPermission.value,
-      },
-    },
-  )
-    .then((value) => {
-      data.value.unshift(value);
-      useMessage({
-        type: 'success',
-        content: `添加角色${value.name}成功`,
-      });
-      addedPermission.value = [];
-      role.name = '';
-      role.desc = '';
-    })
-    .catch((err) => {
-      useMessage({
-        type: 'success',
-        content: err.data.message,
-      });
-    });
-};
-
-const getRoleInfo = (id: number) => {
-  $fetch(`/api/role/${id}`, { method: 'get' })
-    .then((roleInfo) => {
-      if (!roleInfo) {
-        showDialog.value = false;
-        return;
-      }
-      role.name = roleInfo.name;
-      role.desc = roleInfo.desc;
-      addedPermission.value = roleInfo.permission.map(v => v.id);
-      activeId.value = roleInfo.id;
-      showDialog.value = true;
-    });
-};
-
-watch(payload, () => {
-  if (!payload.value) {
-    data.value = [];
-    return;
-  }
-  data.value = payload.value.data;
-}, { immediate: true, deep: true });
+const { role, permissionsInput, addRole, removeUser, patchRole, detailPatch, getRoleInfo, data, totalItems, page, isDialogOpen, closeDialog } = useRole();
 </script>
 
 <template>
@@ -207,7 +41,7 @@ watch(payload, () => {
               权限选择
             </label>
             <app-tag-select
-              v-model="addedPermission"
+              v-model="role.permissions"
               :options="permissionsInput"
               :max-height="128"
               multiple
@@ -285,9 +119,14 @@ watch(payload, () => {
         @page-update="(cur) => page=cur"
       />
     </div>
-    <dialog-root v-model:open="showDialog">
+    <dialog-root
+      v-model:open="isDialogOpen"
+      @update:open="(isOpen) => !isOpen ? nextTick(() => closeDialog()) : null"
+    >
       <dialog-portal>
-        <dialog-overlay class="fixed top-0 left-0 w-full h-full z-30 bg-black/30">
+        <dialog-overlay
+          class="fixed top-0 left-0 w-full h-full z-30 bg-black/30"
+        >
           <dialog-content class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[80vh] max-h-[95vh] w-full p-4 bg-default-200 rounded">
             <dialog-title class="text-2xl mb-4">
               <!-- TODO: I18N -->
@@ -320,7 +159,7 @@ watch(payload, () => {
                   权限选择
                 </label>
                 <app-tag-select
-                  v-model="addedPermission"
+                  v-model="role.permissions"
                   :options="permissionsInput"
                   :max-height="128"
                   multiple
